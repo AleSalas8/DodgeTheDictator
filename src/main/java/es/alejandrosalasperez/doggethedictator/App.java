bd49b40acaf6ddec8620594942bc1d98e995dc4f
1DAW03 <1DAW03@DESKTOP-GRAFEAH>
2020-02-03 10:30:13 +0100

    Imagenes fondo y avion

--- src/main/java/es/alejandrosalasperez/doggethedictator/App.java
+++ src/main/java/es/alejandrosalasperez/doggethedictator/App.java
@@ -1,8 +1,12 @@
 package es.alejandrosalasperez.doggethedictator;
 
 import javafx.application.Application;
+import static javafx.print.Paper.C;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -13,13 +17,20 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        Pane root = new Pane();
+        var scene = new Scene(root, 870, 580);
         stage.setScene(scene);
         stage.show();
+        
+        Image image1 = new Image(getClass().getResourceAsStream("/Imagenes/Fondo.png"));
+        ImageView imageView1 = new ImageView(image1);
+        root.getChildren().add(imageView1);
+         Image image2 = new Image(getClass().getResourceAsStream("/Imagenes/Fondo.png"));
+        ImageView imageView2 = new ImageView(image2);
+        root.getChildren().add(imageView2);
+        Image image3 = new Image(getClass().getResourceAsStream("/Imagenes/aeroplano.png"));
+        ImageView imageView3 = new ImageView(image3);
+        root.getChildren().add(imageView3);
     }
 
     public static void main(String[] args) {
